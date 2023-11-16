c761c664411744b927f750bad062a6b63f74da7f
Pavlo Nahurnyi <polusnp@gmail.com>
2023-11-17 01:08:16 +0200

    added functional buttons

--- src/components/todos/Todo.js
+++ src/components/todos/Todo.js
@@ -1,11 +1,24 @@
-import { FcTodoList } from "react-icons/fc"
+import { RiTodoFill, RiDeleteBin2Line } from "react-icons/ri"
+import { FaCheck } from "react-icons/fa"
 import styles from "./Todo.module.css"
 
-function Todo({ todo, deleteTodo }) {
+function Todo({ todo, deleteTodo, toggleTodo }) {
   return (
-    <div onDoubleClick={() => deleteTodo(todo.id)} className={styles.todo}>
-      <FcTodoList className={styles.todoIcon} />
+    <div
+      className={`${styles.todo} ${
+        todo.isCompleted ? styles.completedTodo : ""
+      }`}
+    >
+      <RiTodoFill className={styles.todoIcon} />
       <div className={styles.todoText}>{todo.text}</div>
+      <RiDeleteBin2Line
+        className={styles.deleteIcon}
+        onClick={() => deleteTodo(todo.id)}
+      />
+      <FaCheck
+        className={styles.checkIcon}
+        onClick={() => toggleTodo(todo.id)}
+      />
     </div>
   )
 }
